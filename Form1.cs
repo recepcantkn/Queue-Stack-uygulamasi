b9232bc56d3a061acc3dc6d49c75057855824151
Recep Can <61096668+recepcantkn@users.noreply.github.com>
2022-03-07 11:37:36 +0300

    kodlarim

---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Queue_form_tasarımı_uygulaması_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Queue kuyruk = new Queue();
+        int sira = 0;
+
+        private void btnSiraAl_Click(object sender, EventArgs e)
+        {
+            sira++;
+            kuyruk.Enqueue(sira);
+            Listele();
+        }
+        private void Listele()
+        {
+            listeSiralar.Items.Clear();
+            foreach (int sira in kuyruk)
+            {
+                listeSiralar.Items.Add(sira);
+            }
+        }
+
+        private void btnSiraCikart_Click(object sender, EventArgs e)
+        {
+            kuyruk.Dequeue();
+            Listele();
+        }
+         
+    }
+}
